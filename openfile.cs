da675dc8c8f320af1b4d009163a39d3b1b07a442
CobraG <515882741@qq.com>
2017-08-09 11:22:01 +0800

    the first test

---
+++ openfile.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Diagnostics;
+
+public class openfile : MonoBehaviour 
+{
+    //string paths = @"E:\WinToDog\Titles.EXE";
+  
+        void Awake()
+    {
+            File.CreateText(Application.streamingAssetsPath + @"\Invalidcode.txt");
+    }
+    
+    void Start()
+    {
+        string paths = Application.streamingAssetsPath + @"\ConsoleApplication1.exe";
+        Application.OpenURL(paths);
+        StartCoroutine(OpenF());
+    }
+
+
+    IEnumerator OpenF() {
+        string thefliepaths = Application.streamingAssetsPath + @"\Invalidcode.txt";
+        yield return new WaitForSeconds(1f);
+        if (ReadFilesto() == "0x000000")
+            File.WriteAllText(thefliepaths, "运行没有错误");
+        else
+        {
+            StopAllCoroutines();
+            Application.Quit();
+        }
+    }
+
+    private string  ReadFilesto()
+    {
+        string thefliepaths = Application.streamingAssetsPath + @"\Invalidcode.txt";
+        string t=File.ReadAllText(thefliepaths);
+        return t;
+    }
+    
+}
+
